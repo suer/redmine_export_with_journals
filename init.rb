38ffaead6948826796e7b9d0586d62775ddbba7b
suer <sue@nagoya-u.jp>
2010-10-02 21:40:52 +0900

    init

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_export_with_journals do
+  name 'Redmine Export With Journals plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
