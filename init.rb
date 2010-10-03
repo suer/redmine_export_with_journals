e14fdf0ff30718614650959259692956a06c716a
suer <sue@nagoya-u.jp>
2010-10-03 22:52:28 +0900

    add description

--- init.rb
+++ init.rb
@@ -3,11 +3,10 @@ require 'export_with_journals'
 
 Redmine::Plugin.register :redmine_export_with_journals do
   name 'Redmine Export With Journals plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'suer'
+  description 'Export CSV with Journals'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
-
+  url 'http://github.com/suer/redmine_export_with_journals'
+  author_url 'http://d.hatena.ne.jp/suer'
   permission :export_csv_with_journals, {:issues_export => :export_with_journals}
 end
