1bd06cd04d544df423d022434dafbbf6d8daece0
suer <sue@nagoya-u.jp>
2010-10-03 23:39:45 +0900

    display attrs when changed

--- app/helpers/issues_export_helper.rb
+++ app/helpers/issues_export_helper.rb
@@ -5,7 +5,9 @@ module IssuesExportHelper
         if row.header_row?
           newcsv << row.fields + [t(:label_history)]
         else
-          newcsv << row.fields + Issue.find(row.fields.first).journals.map {|j| j.user.name + "\n" + (j.notes.nil? ? '' :     j.notes)}
+          newcsv << row.fields + Issue.find(row.fields.first).journals.map do |j| 
+            j.user.name + "\n" + j.details.map {|d| show_detail(d, true)}.join("\n") + "\n" + (j.notes.nil? ? '' :     j.notes)
+          end
         end
       end
     end.tosjis
