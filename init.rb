abc500966120c3592e05513e2d67e8d397d85f23
Ryo SUETSUGU <suetsugu.r@gmail.com>
2024-06-11 05:14:28 +0900

    Bumped: Ver.0.3.0

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_export_with_journals do
   name 'Redmine Export With Journals plugin'
   author 'suer'
   description 'Export CSV with Journals'
-  version '0.2.0'
+  version '0.3.0'
   url 'https://github.com/suer/redmine_export_with_journals'
   author_url 'http://d.hatena.ne.jp/suer'
   permission :export_csv_with_journals, {:issues_export => :export_with_journals}
